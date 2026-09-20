f93a30c6dbb9b5dd5cbde5e74d03ee1ba756ef9f
Niamh Brady <niamhbrady6114@gmail.com>
2026-09-20 22:20:21 +0100

    Created & formatted the form, added admin to update and delete records.

---
+++ neuromodulation_select.php
@@ -0,0 +1,125 @@
+<?php 
+
+    require_once __DIR__ . '/db.php';  
+    
+
+    $messasge = "";
+    $messageType = "";
+    $results = array();
+
+
+    $id = (isset($_GET['id']) ? $_GET['id']: NULL);
+    
+    // SELECT/GET RECORDS
+    $tsql = "{call sp_select_neuromodulation (?)}"; 
+    $params = array(
+        array(NULL, SQLSRV_PARAM_IN)
+    );
+
+    $stmt = sqlsrv_prepare($conn, $tsql, $params);
+    if ($stmt && sqlsrv_execute($stmt)) {
+        while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+            if ($row['submission_date'] instanceof DateTime) {
+                $row['submission_date'] = $row['submission_date']->format('d/m/Y');
+            }
+            if ($row['birth_date'] instanceof DateTime) {
+                $row['birth_date'] = $row['birth_date']->format('d/m/Y');
+            }
+            $results[] = $row;
+        }
+    } else {
+        $message = "Error Submitting Form: " . print_r(sqlsrv_errors(), true);
+        $messageType = "danger";
+    }
+
+    // DELETE RECORD
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        $action = $_GET['action'] ?? '';
+
+        if ($action === 'delete') {
+
+            $tsql = "{call sp_delete_neuromodulation (?)}";
+            $params = array(
+                array($id, SQLSRV_PARAM_IN)
+            );
+
+            $stmt = sqlsrv_prepare($conn, $tsql, $params);
+            if ($stmt && sqlsrv_execute($stmt)) {
+                $message = "Form Successfully Deleted.";
+                $messageType = "success";
+            } else {
+                $message = "Error Deleting Form: " . print_r(sqlsrv_errors(), true);
+                $messageType = "danger";
+            }
+            
+        }
+    } else {
+        echo $_SERVER['REQUEST_METHOD'];
+    }
+?>
+
+
+<html>
+     <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Neuromodulation</title>
+        <!-- BOOTSTRAP 5 CSS (CDN) -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/7ffc81d928.js" crossorigin="anonymous"></script>
+    </head>
+
+    <body>
+        <?php include_once "header.php" ?>
+        <div class="container-fluid">
+            <?php if(!empty($message)) { ?>
+                <div class="row align-items-start pt-3">
+                    <div class="col">
+                            <div class="alert alert-<?= $messageType; ?>" role="alert"><?= $message; ?></div>
+                    </div>
+                </div>
+            <?php } ?>
+            <div class="row pt-3">
+                    <div class="col-12">   
+                        <div class="card">
+                            <div class="card-header">Admin</div>
+                            <div class="card-body"> 
+                                <input type="hidden" name="action" value="delete">
+                                <table class="table table-striped">
+                                    <thead>
+                                        <tr>
+                                            <td class="text-center">Submission Date</td>
+                                            <td>First Name</td>
+                                            <td>Surname</td>
+                                            <td class="text-center">Age</td>
+                                            <td class="text-center">Date of Birth</td>
+                                            <td class="text-center">Total Score</td>
+                                            </td>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <!-- Looping the results -->
+                                        <?php foreach($results as $result){ ?> 
+                                            <tr>
+                                                <td class="text-center"><?=  $result["submission_date"] ?></td>
+                                                <td><?=  $result["first_name"] ?></td>
+                                                <td><?=  $result["surname"] ?></td>
+                                                <td class="text-center"><?=  $result["age"] ?></td>
+                                                <td class="text-center"><?=  $result["birth_date"] ?></td>
+                                                <td class="text-center"><?=  $result["total_score"] ?></td>
+                                                <td>
+                                                    <a href="/neuromodulation_select.php?action=delete&id=<?=  $result["id"] ?>"><i class="fa-solid fa-trash-can"></i></a>
+                                                    <a href="/neuromodulation_update.php?id=<?=  $result["id"] ?>"><i class="fa-solid fa-pen-to-square"></i></a>
+                                                </td>
+                                            </tr>
+                                        <?php } ?>
+                                    </tbody>
+                                </table>  
+                    
+                            </div>
+                        </div>  
+                    </div> 
+            </div>
+        </div>
+    </body>
+</html>
